8abc280ab661bf0ed160187065f6c22c9806fae2
HP <HP@10.41.171.118>
2024-02-01 12:30:39 +0530

    First Commit

---
+++ TNSIFassignments/src/com/tnsif/day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.tnsif.day1;
+
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome!");
+		System.out.print("CSE!");
+	}
+
+}
---
+++ TNSIFassignments/src/com/tnsif/dayfour/secondpackage/Executer.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayfour.secondpackage;
+
+import com.tnsif.dayfour.firstpackage.Base;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		
+		Base obj = new Base();
+		
+		obj.publicMethod();
+		
+
+	}
+
+}
